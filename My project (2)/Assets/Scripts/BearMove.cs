dde575b51952d43b1fec2807df934131948f5d18
Rui Zhu <91820832+ruizhu0400@users.noreply.github.com>
2026-03-03 04:29:27 -0500

    Add Ice Mine 3 and movable bears

---
+++ My project (2)/Assets/Scripts/BearMove.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+
+public class BearMove : MonoBehaviour
+{
+    public bool move = true;
+    public char axis = 'x';
+    private float amplitude = 1f; 
+    private float frequency = 1f; 
+
+    private Vector2 startPosition;
+
+    void Start()
+    {
+        startPosition = transform.position;
+        amplitude = Random.Range(1f, 1.5f);
+        frequency = Random.Range(5f, 8f); 
+        int randomAxis = Random.Range(0, 2);
+        if (randomAxis == 1) axis = 'y';
+    }
+
+    void Update()
+    {
+        if (move) {
+            float offset = Mathf.Sin(Time.time * frequency) * amplitude;
+
+            transform.position = new Vector2(
+                startPosition.x + (axis == 'x' ? offset : 0),
+                startPosition.y + (axis == 'y' ? offset : 0)
+            );
+        }
+    }
+}
